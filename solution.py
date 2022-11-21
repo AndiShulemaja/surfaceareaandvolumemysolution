43f64532b65a8dc2262d30e2ea8f4118f17f7fec
AndiShulemaja <118752976+AndiShulemaja@users.noreply.github.com>
2022-11-21 15:36:25 +0000

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,36 @@
+Count = 0
+while (Count !=3) or (would_you_like_to_continue=="yes") or ((Character !="b") and (Character !="c") and (Character !="e")):
+        Character = input("Enter a character")
+        if (Character == "C") or (Character == "B") or (Character=="c") or (Character=="b"):
+            Count += 1
+            Width=int(input("Enter Width"))
+            Height=int(input("Enter Height"))
+            Length=int(input("Enter Length"))
+            Volume=(Width*Height*Length)
+            Surface_Area=2*((Height*Width)+(Height*Length)+(Width*Length))
+            print(Volume)
+            print(Surface_Area)
+            if Count==3:
+                would_you_like_to_continue=input("Would you like to continue?")
+                if would_you_like_to_continue=="yes":
+                        Count=0
+
+        elif (Character=="e") or (would_you_like_to_continue=="no"):
+                break
+
+    
+
+     
+
+
+
+
+
+        
+
+
+
+
+
+
+
